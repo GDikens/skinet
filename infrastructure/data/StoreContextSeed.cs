ab0c22aec596d58ed94847f226bb7eb40b30a294
Dineth Kariyawasam <dinethkariyawasam@gmail.com>
2024-10-06 08:55:58 +0530

    End of section 3

---
+++ infrastructure/data/StoreContextSeed.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text.Json;
+using core.entities;
+
+namespace infrastructure.data;
+
+public class StoreContextSeed
+{
+    public static async Task SeedAsync(StoreContext context)
+    {
+        if (!context.Products.Any())
+        {
+            var productsData = await File.ReadAllTextAsync("../infrastructure/data/seed_data/products.json");
+            var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+            if (products == null) return;
+
+            context.Products.AddRange(products);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
